ab7d9ed2e6d0e3d8698a435aab5e0030f78af226
Лев Курас <levkuras@yandex.kz>
2024-12-16 13:18:22 +0100

    Удален неиспользуемый импорт

--- server/src/test/java/ru/practicum/shareit/controllers/UserControllerTests.java
+++ server/src/test/java/ru/practicum/shareit/controllers/UserControllerTests.java
@@ -23,7 +23,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
